16b03a3a34dc61187cda7928c99550cbcf174861
Caleb Eggensperger <calebegg@gmail.com>
2011-11-10 19:23:35 +0000

    More style changes; added shadow when console is scrolled down.

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 window.onresize = resize;
-sid = Math.random()
+sid = Math.random();
 prev = $('<input id="prev" type="button" value="&lt;" />');
 next = $('<input id="next" type="button" value="&gt;" />');
 function resize() {
   pre_width = pre.width();
+  console.log(pre_width);
+  if (pre_width > 800) {
+    setTimeout(resize, 100);
+    return;
+  }
   outer_console.width(pre_width + 10);
   tutorial.width(body.width() - pre_width + 10);
   info.css('right', pre_width - info.width() - 35 + 'px');
   jq_console.height(outer_console.height() - $('h1').height() - 10);
 }
+opacity = 0;
 function main() {
   jq_console = $('#jq-console');
   outer_console = $('#console');
@@ -23,12 +29,26 @@ function main() {
   prev.click(function(){alert('prev');});
   next.click(function(){alert('next');});
   throbber = $('#throbber');
+  // Shadow effect on scrolled console
+  function consoleShadowScrollHandler() {
+    if (this.scrollTop > 0) {
+      old_opacity = opacity;
+      opacity = Math.min(this.scrollTop, 20) / 20 / 6 + .05;
+      if (opacity != old_opacity) {
+        outer_console.css('box-shadow', "2px 2px 8px rgba(0, 0, 0, .2), " +
+          " 0 15px 10px -10px rgba(0,0,0," + opacity + ") inset");
+      }
+    } else {
+      outer_console.css('box-shadow', '');
+    }
+  }
+  outer_console.scroll(consoleShadowScrollHandler);
   controller = jq_console.console(
     {
       autofocus: true,
       promptLabel: '> ',
       commandValidate: function(line) {
-        if (line == "") {
+        if (line === "") {
           return false;
         } else {
           return true;
@@ -66,7 +86,7 @@ function main() {
           outer_console_elem = outer_console[0];
           outer_console_elem.scrollTop = outer_console_elem.scrollHeight;
         });
-      },
+      }
   });
   // Add links to <code> elements.
   $('code').each(function() {
