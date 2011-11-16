10988aa6f931efbf67098e91e91e63ccea1e212d
Caleb Eggensperger <calebegg@gmail.com>
2011-11-16 10:00:54 -0600

    Added command blacklist, fixed a few bugs, added detailed logging.

--- acl2-handler.py
+++ acl2-handler.py
@@ -1,28 +1,46 @@
 #!/usr/bin/python
 
 import BaseHTTPServer
+import time
+import logging
+
 from threading import Timer, Semaphore, Lock
 from subprocess import Popen, PIPE
 from urllib import unquote
 
 import acl2
 
+MAX_SESSION_LENGTH = 60 * 60 # One hour
+FILES = "jquery.console.js script.js style.css throbber.gif favicon.ico".split()
 INITIAL_ACL2_COUNT = 10
 
+logging.basicConfig(format="[%(asctime)s | %(levelname)s]: %(message)s",
+                    filename="handler.log",
+                    level=logging.DEBUG)
+logger = logging.getLogger("acl2-handler")
 a = {}
 acl2_queue = [acl2.ACL2() for i in range(INITIAL_ACL2_COUNT)]
 acl2_queue_semaphore = Semaphore(INITIAL_ACL2_COUNT)
 acl2_queue_lock = Lock()
 
+def remove_old_instances():
+  logger.debug("Removing old instances.")
+  now = time.time()
+  with acl2_queue_lock:
+    old_keys = []
+    for key, instance in a.iteritems():
+      if instance.last_command_at < now - MAX_SESSION_LENGTH:
+        old_keys.append(key)
+    for key in old_keys:
+      logger.info("Removing old session with key: " + key)
+      a[key].close()
+      del a[key]
+
 def create_new_acl2():
-  print 'creating new acl2'
-  acl2_queue_lock.acquire()
-  print 'got lock'
-  acl2_queue.append(acl2.ACL2())
-  print 'done creating'
-  acl2_queue_semaphore.release()
-  acl2_queue_lock.release()
-  print 'done'
+  logger.debug("Trying to create a new ACL2 instance.")
+  with acl2_queue_lock:
+    acl2_queue.append(acl2.ACL2())
+    acl2_queue_semaphore.release()
 
 class ACL2Handler(BaseHTTPServer.BaseHTTPRequestHandler):
   def file_handle(self, fn):
@@ -33,40 +51,60 @@ class ACL2Handler(BaseHTTPServer.BaseHTTPRequestHandler):
     self.wfile.flush()
     index.close()
   def do_GET(self):
+    logger.debug("Handling GET request: " + self.path)
     if self.path == '/':
       self.file_handle('index.html')
       return
-    elif self.path == '/jquery.console.js':
-      self.file_handle('jquery.console.js')
+    elif self.path[1:] in FILES:
+      self.file_handle(self.path[1:])
       return
-    elif self.path == '/script.js':
-      self.file_handle('script.js')
-      return
-    elif self.path == '/style.css':
-      self.file_handle('style.css')
-      return
-    elif self.path == '/throbber.gif':
-      self.file_handle('throbber.gif')
-      return
+    # Otherwise, look for the code parameter.
     params_path = self.path.split('?')
     if len(params_path) == 1:
       self.send_error(500, 'Missing required "code" parameter')
       return
+    logger.debug("request contains params")
     params = dict([p.split('=') for p in params_path[1].split('&')])
     code = params['code']
     sid = params.get('sid', 0)
+    logger.debug("GET request contains correct code and sid params")
+    Timer(10, remove_old_instances).start()
+    logger.debug("Started instance removal timer")
     if not sid in a:
+      if len(acl2_queue) + len(a) < 100:
+        Timer(5, create_new_acl2).start()
+      else:
+        logger.error("Out of room for more ACL2 instances")
+      logger.debug("Getting a new ACL2. Remaining: {0}".format(len(acl2_queue)))
       acl2_queue_semaphore.acquire()
-      acl2_queue_lock.acquire()
-      a[sid] = acl2_queue.pop()
-      acl2_queue_lock.release()
-      Timer(5, create_new_acl2).start()
+      with acl2_queue_lock:
+        a[sid] = acl2_queue.pop()
+    logger.debug("Getting code ready for processing.")
     code = code.replace('+', ' ')
     code = unquote(code)
-    output = a[sid].issue_form(code)
+    logger.debug("Code for processing: {0}".format(code))
+    try:
+      logger.debug("Processing code")
+      output = a[sid].issue_form(code)
+      logger.debug("Successfully processed code")
+    except acl2.BlacklistException as e:
+      logger.debug("Blacklist exception: {0}".format(e))
+      self.send_response(200, str(e))
+      self.wfile.write('\n Error: ' + str(e))
+      self.wfile.close()
+      return
+    except acl2.TerminatedException as e:
+      logger.warning("Terminated exception: {0}".format(e))
+      self.send_response(200, str(e))
+      self.wfile.write('\n Error: {0}'.format(e))
+      del a[sid]
+      self.wfile.close()
+      return
+    logger.debug("Code ran successfully, outputing.")
     self.send_response(200, '')
     self.wfile.write('\n' + output)
     self.wfile.close()
+    logger.debug("Done outputting response.")
 
 httpd = BaseHTTPServer.HTTPServer(('', 8000), ACL2Handler)
 httpd.serve_forever()
--- acl2.py
+++ acl2.py
@@ -1,10 +1,43 @@
 #!/usr/bin/python
 
 from subprocess import Popen, PIPE
+import logging
 import re
 import sys
+import time
 
 import parse_sexp
+
+logger = logging.getLogger()
+
+class TerminatedException(Exception): pass
+class BlacklistException(Exception): pass
+
+def check_none_in_blacklist(code, top_level=True):
+  blacklist = '''
+  progn! progn pprogn set-raw-mode :set-raw-mode trace open-output-channel er
+  error1 er-progn fms fms! fmt! fmt fmt1 fmt1! getenv$
+  get-output-stream-string$ illegal mbe mbe1 mbt open-input-channel-p
+  close-input-channel close-output-channel open-output-channel-p read-byte$
+  read-char$ read-object setenv$ standard-co standard-oi sys-call the
+  with-live-state ec-call comp defexec defproxy defun-nx make-event memoize
+  profile redo-flat set-body unmemoize include-book break-on-error
+  close-trace-file open-trace-file trace! trace$ wet untrace$
+  install-new-raw-prompt
+  '''.split()
+  for term in code:
+    if isinstance(term, list):
+      if len(term) > 0 and term[0] in blacklist:
+        raise BlacklistException(term[0] + " is not allowed here for " +
+            "security reasons. Download ACL2 to try this feature.")
+      else:
+        check_none_in_blacklist(term, False)
+    elif isinstance(term, str):
+      if top_level and (term in blacklist or ':' + term in blacklist):
+        raise BlacklistException(term + " is not allowed here for security " +
+            "reasons. Download ACL2 to try this feature.")
+    else:
+      raise BlacklistException("Encountered an unknown problem processing " + term)
 
 class ACL2(object):
   def __init__(self):
@@ -14,14 +47,20 @@ class ACL2(object):
     self.ins = self.acl2.stdin
     self.out = self.acl2.stdout
     self.expect_prompts = 1
+    self.last_command_at = time.time()
     self.issue_form('()')
   def issue_form(self, form):
+    logger.debug("Entering issue_form")
     if self.closed:
-      raise Exception("Already terminated ACL2.")
+      raise TerminatedException("Already terminated ACL2.")
+    self.last_command_at = time.time()
     parsed_code = parse_sexp.parse(form)
+    logger.debug("parsed code")
+    check_none_in_blacklist(parsed_code)
     prev_was_key = False
     for term in parsed_code:
-      if isinstance(term, list) or not re.match(r'^\s*$', term):
+      if isinstance(term, list) or not (re.match(r'^\s*$', term) or 
+                                        term == "'"):
         if prev_was_key:
           prev_was_key = False
         else:
@@ -31,6 +70,8 @@ class ACL2(object):
     self.ins.write(form + '\n');
     ret = []
     buff = [''] * 8
+    logger.debug("Writing to ACL2, waiting for {0} prompts"
+                    .format(self.expect_prompts))
     while self.expect_prompts > 0:
       char = self.out.read(1)
       buff.pop(0)
@@ -40,12 +81,17 @@ class ACL2(object):
           buff == ['\n', 'A', 'C', 'L', '2', ' ', 'p', '>'] or
           buff == ['\n', 'A', 'C', 'L', '2', ' ', 'p', '>'] or
           buff == ['A', 'C', 'L', '2', ' ', 'p', '!', '>']):
+        logger.debug("Got a prompt")
         ret = ret[:-7]
         self.expect_prompts-= 1
         if buff[0] != '\n':
           ret.append(char)
       else:
         ret.append(char)
+    result = ''.join(ret)
+    if re.search(r'ABORTING from raw Lisp', result):
+      self.close()
+      raise TerminatedException("ACL2 terminated because of malformed input.")
     return ''.join(ret)
   def close(self):
     if self.closed:
@@ -53,7 +99,8 @@ class ACL2(object):
     self.acl2.communicate()
     self.closed = True
   def __del__(self):
-    self.close()
+    if not self.closed:
+      self.close()
 
 if __name__ == '__main__':
   acl2 = ACL2()
